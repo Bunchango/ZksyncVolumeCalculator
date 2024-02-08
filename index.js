810ec25dcf4a1f4f2c65064469ff2628d74406a7
Marcuss <119033041+Bunchango@users.noreply.github.com>
2024-02-08 06:15:39 -0800

    Add files via upload
    run from terminal

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer");
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-const getData = async () => {
-    // Start a Puppeteer session with:
-    // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-    // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+const getData = async (address) => {
     const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
+        headless: false,
+        defaultViewport: null,
     });
-  
-    // Open a new page
+
     const page = await browser.newPage();
-  
-    // On this new page:
-    const address = "0xa351b9F4BEE0e87660fc080817ad5Fa84E9CdA8E"
+
     await page.goto("https://explorer.zksync.io/address/" + address, {
-      waitUntil: "networkidle0",
+        waitUntil: "networkidle0",
     });
-  
+
     let total = 0;
 
-
     while (true) {
-
-        // Scrape the data on the current page
         const vol = await page.evaluate(() => {
             const elements = document.querySelectorAll('.table-container.has-head.has-footer.transactions-table.high-rows.transactions-table .token-price');
             return Array.from(elements, el => el.innerHTML);
         });
 
-        // Calculate the total amount
         vol.forEach(val => {
             total += parseFloat(val.replace("$", ""));
         });
@@ -41,23 +31,27 @@ const getData = async () => {
         const isDisabled = await page.$eval('.pagination-page-button.arrow.right', button => {
             return button.classList.contains('disabled');
         });
-        
+
         if (!isDisabled) {
-            // If the button is not disabled, click it
             await page.click('.pagination-page-button.arrow.right');
             await delay(500);
         } else {
-            // If disabled, break the loop
             break;
         }
     }
-    
-    console.log(`Your total volume is: `, Math.round(total, 2));
 
-    // Close the browser
+    console.log(`Total volume for address ${address}: `, total.toFixed(2));
+
     await browser.close();
 };
 
-// Start the scraping
-getData();
-  
+// Extracting command line arguments
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.error("Usage: node script.js <address>");
+    process.exit(1);
+}
+
+const address = args[0];
+
+getData(address);
